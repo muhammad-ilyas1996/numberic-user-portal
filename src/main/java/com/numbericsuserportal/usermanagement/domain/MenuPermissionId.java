65dd795906174dfd36194ec6d6dd160490a4d8ef
ilyas-acc <125976254+ilyas-acc@users.noreply.github.com>
2025-11-23 17:29:07 +0500

    commit

---
+++ src/main/java/com/numbericsuserportal/usermanagement/domain/MenuPermissionId.java
@@ -0,0 +1,34 @@
+package com.numbericsuserportal.usermanagement.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Embeddable
+public class MenuPermissionId implements Serializable {
+    
+    private Long menuId;
+    private Long permissionId;
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuPermissionId that = (MenuPermissionId) o;
+        return Objects.equals(menuId, that.menuId) && Objects.equals(permissionId, that.permissionId);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(menuId, permissionId);
+    }
+}
+
+
---
+++ src/main/java/com/numbericsuserportal/usermanagement/domain/Role.java
@@ -0,0 +1,49 @@
+package com.numbericsuserportal.usermanagement.domain;
+
+import com.numbericsuserportal.commonpersistence.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import com.numbericsuserportal.usermanagement.domain.PortalType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role  extends BaseEntity{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long roleId;
+    
+    @Column(name = "code_name", nullable = false, length = 50)
+    private String codeName;
+    
+    @Column(name = "display_name", nullable = false, length = 100)
+    private String displayName;
+    
+    @Column(name = "description", nullable = false, length = 200)
+    private String description;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portal_type_id", nullable = false)
+    private PortalType portalType;
+    
+    @Column(name = "is_superadmin", nullable = false)
+    private Boolean isSuperadmin = false;
+    
+    @Column(name = "is_readonly", nullable = false)
+    private Boolean isReadonly = false;
+    
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<RolePermission> rolePermissions = new HashSet<>();
+    
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<UserRole> userRoles = new HashSet<>();
+}
